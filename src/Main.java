63785245f4cd8e10911d435482653aa97cbf88fc
hjnino <heidyninoavila@gmail.com>
2024-08-09 17:11:59 -0500

    Inclui mi nombre

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("hola mundo");
+        System.out.println("Edinson Martinez");
 
-
+        System.out.println("Heidy Nino");
 
     }
 
